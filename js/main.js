208213c2355e51d90c7eacd6194b9644086417e8
Dominique Summers <domo@Dominiques-MacBook-Pro.local>
2020-10-17 19:10:52 -0700

    Added more validation

--- js/main.js
+++ js/main.js
@@ -3,8 +3,27 @@ window.onload = function () {
     formBtn.onclick = main;
 };
 function main() {
+    resetErrorMessages();
     validateTxtInput("first-name", "First name is required");
     validateTxtInput("last-name", "Last name is required");
+}
+function resetErrorMessages() {
+    var allSpans = document.querySelectorAll("form span");
+    for (var i = 0; i < allSpans.length; i++) {
+        var currSpan = allSpans[i];
+        if (currSpan.hasAttribute("data-required")) {
+            currSpan.innerText = "";
+        }
+        else {
+            if (currSpan.hasAttribute("data-default-error")) {
+                var nsg = currSpan.getAttribute("data-default-error");
+                currSpan.innerText = "";
+            }
+            else {
+                currSpan.innerText = "";
+            }
+        }
+    }
 }
 function validateTxtInput(id, errMsg) {
     var txtBox = document.getElementById(id);
--- ts/main.ts
+++ ts/main.ts
@@ -6,10 +6,37 @@ window.onload = function(){
 }
 
 function main():void{
+    resetErrorMessages();
    validateTxtInput("first-name", "First name is required");
    validateTxtInput("last-name", "Last name is required");
 
 }
+/**
+ * Resets all the spans back to the default text
+ *
+ */
+function resetErrorMessages():void{
+    let allSpans = document.querySelectorAll("form span");
+    for(let i = 0; i < allSpans.length; i++){
+        let currSpan = <HTMLElement>allSpans[i];
+
+        if(currSpan.hasAttribute("data-required")){
+            currSpan.innerText = "";
+        }
+        else{
+            if (currSpan.hasAttribute("data-default-error")){
+                let nsg = currSpan.getAttribute("data-default-error");
+                currSpan.innerText = "";
+            }
+            else{
+                currSpan.innerText = "";
+
+            }
+        }
+    }
+    
+}
+
 
 /**
  * Returns true if the textbox with the given id
